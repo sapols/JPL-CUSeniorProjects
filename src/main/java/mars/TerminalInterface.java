d4707907dfe1978137360c9333957188a93dadfe
Robert Ballard <robert.ballard@colorado.edu>
2017-12-06 14:26:40 -0700

    Rough documentation pass for TerminalInterface. Will revisit

--- src/main/java/mars/TerminalInterface.java
+++ src/main/java/mars/TerminalInterface.java
@@ -5,19 +5,28 @@ import org.omg.IOP.CodecPackage.TypeMismatch;
 
 import java.util.Scanner;
 
+// TODO revisit after wrapper function commit
+
 /**
  * Class through which users interact with out project.
  * At start up, "promptUser" asks the user for rover specifications
  * (including which algorithm to run, and start/end coordinates).
  * A rover with those specifications is then started and the program runs until completed.
+ *
+ * TODO properly format with tags -- may not be possible due to how javadoc works
+ * slope: maxSlope for input rover
+ * startCoords: starting coordinates for input rover
+ * endCoords: ending coordinates for input rover
+ * mapPath: file path to a terrain map
+ * alg: string used to determine what algorithm user chooses
+ * fieldOfView:
  */
 public class TerminalInterface extends UserInterface{
-
     double slope = 0;
     int[] startCoords = {0, 0};
     int[] endCoords = {0, 0};
     String mapPath = "";
-    String alg = ""; //used to determine Algorithm to use
+    String alg = "";
     double fieldOfView = 0;
     TerrainMap m = new GeoTIFF();
 
@@ -28,16 +37,15 @@ public class TerminalInterface extends UserInterface{
         System.out.println("Welcome to the Martian Autonomous Routing System");
         System.out.println("Please enter the path for the map you would like to traverse");
 
-        while(true) {
-            try{
+        while(true) { // get mapPath
+            try {
                 mapPath = scanner.next();
                 m.initMap(mapPath);
                 break;
-            }catch(TypeMismatchException e) {
+            } catch(TypeMismatchException e) {
                 System.out.println("Warning: Please enter a string");
                 scanner.nextLine();
-            }catch(Exception e)
-            {
+            } catch(Exception e) {
                 System.out.println("Warning: Make sure the path you are entering is correct");
                 scanner.nextLine();
             }
@@ -45,46 +53,43 @@ public class TerminalInterface extends UserInterface{
         }
         scanner.nextLine();
         System.out.println("Please enter the max slope your rover can handle");
-        while(true) {
+        while(true) { // get maxSlope
             try {
-
                 slope = scanner.nextDouble();
                 break;
             } catch (Exception e) {
-
                 System.out.println("Warning: Please enter a number");
                 scanner.nextLine();
             }
         }
 
         System.out.println("Enter Start Coordinates");
-        while(true) {
+        while(true) { // get startCoords
             try {
                 startCoords[0] = scanner.nextInt();
                 startCoords[1] = scanner.nextInt();
                 break;
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Warning: Enter coordinates as ints");
                 scanner.nextLine();
             }
         }
         scanner.nextLine();
         System.out.println("Enter end Coordinates");
-        while(true) {
+        while(true) { // get endCoords
             try {
                 endCoords[0] = scanner.nextInt();
                 endCoords[1] = scanner.nextInt();
                 break;
-            } catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Warning: Enter Coordinates as ints");
                 scanner.nextLine();
             }
         }
         scanner.nextLine();
-        while(true) {
+        while(true) { // get alg
             System.out.println("Which algorithm would you like to use, (U)nlimited scope or (L)imited Scope");
-            alg = scanner.next();
+            alg = scanner.next(); //TODO suggest switch statement to implement this
             if(alg.equals("U") || alg.equals("u") ) {
                 startAlgorithm();
                 break;
@@ -92,7 +97,7 @@ public class TerminalInterface extends UserInterface{
             else if(alg.equals("L") || alg.equals("l")) {
                 scanner.nextLine();
                 System.out.println("Enter the Field of View Radius of your rover");
-                while(true) {
+                while(true) { // get fieldOfView
                     try {
                         fieldOfView = scanner.nextDouble();
                         break;
