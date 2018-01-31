bfa7779f6ca3d28fdf29a7bdfc723e10166a8ba0
Ross Blassingame <ross.blassingame@colorado.edu>
2018-01-31 14:32:37 -0700

    save

--- src/main/java/mars/algorithm/Dijkstra.java
+++ src/main/java/mars/algorithm/Dijkstra.java
@@ -4,6 +4,8 @@ import mars.coordinate.Coordinate;
 import mars.coordinate.DijkstraNode;
 import mars.out.TerminalOutput;
 import mars.rover.MarsRover;
+
+import javax.sound.midi.SysexMessage;
 import java.util.*;
 
 public class Dijkstra extends Algorithm {
@@ -28,8 +30,15 @@ public class Dijkstra extends Algorithm {
         double heightOfMap = map.getHeight();
         double widthOfMap = map.getWidth();
 
-        for (int y = 1; y <= heightOfMap; y++) {
-            for (int x = 1; x <= widthOfMap; x++) {
+        int goalX = goalNode.getPosition().getX();
+        int goalY = goalNode.getPosition().getY();
+
+
+        // Instead of loading entire map, let's try only loading the square from start to goal.
+        // For example, if start is 10, 10 and goal is 20, 20, the rectangle will have:
+        // x vals from 10 - 20, and y vals from 10 - 20.
+        for (int y = 1; y <= goalY; y++) {
+            for (int x = 1; x <= goalX; x++) {
                 Coordinate tmpCoordinate = new Coordinate(x,y);
                 DijkstraNode tmpNode = new DijkstraNode(tmpCoordinate);
                 tmpNode.setDistanceFromStart(Double.POSITIVE_INFINITY);
@@ -43,50 +52,28 @@ public class Dijkstra extends Algorithm {
         // Add start node to nodeSet.
         nodeSet.add(startNode);
 
+        double currentMin = Double.POSITIVE_INFINITY;
+        for (DijkstraNode n : nodeSet) {
+            System.out.println(n.getPosition().getX());
+            if (n.getDistanceFromStart() < currentMin) {
+                currentMin = n.getDistanceFromStart();
+            }
+        }
+        System.out.println(currentMin);
+
+        /*
         while (!nodeSet.isEmpty()) {
 
-            // Initialize min dist node.
-            DijkstraNode minDistNode = new DijkstraNode(new Coordinate(-1, -1));
-            minDistNode.setDistanceFromStart(Double.POSITIVE_INFINITY);
-
-            // Get node with min dist from start from nodeSet.
-            double min = Double.POSITIVE_INFINITY;
+            double currentMin = Double.POSITIVE_INFINITY;
             for (DijkstraNode n : nodeSet) {
-                if (n.getDistanceFromStart() < min) {
-                    minDistNode = n;
-                    min = n.getDistanceFromStart();
+                if (n.getDistanceFromStart() < currentMin) {
+                    currentMin = n.getDistanceFromStart();
                 }
             }
 
-            // Remove min node from set.
-            nodeSet.remove(minDistNode);
 
-            for (DijkstraNode n : minDistNode.getNeighbors()) {
-                // Only look at notes we have note visited.
-                if (!minDistNode.hasVisited(n)) {
-                    //  total distance to new node
-                    double totalDistToNewNode = minDistNode.getDistanceFromStart() + minDistNode.distBetween(n);
-
-                    // Shorter path has been found.
-                    if (totalDistToNewNode < n.getDistanceFromStart()) {
-                        n.setDistanceFromStart(totalDistToNewNode);
-                        n.setParent(minDistNode);
-                    }
-
-                    minDistNode.addToVisitedSet(n);
-                }
-            }
-            if (minDistNode.currentIsGoal(goalNode)) {
-                minDistNode.constructPath();
-
-                List<Coordinate> fullPath = minDistNode.getFullPath();
-                Collections.reverse(fullPath);
-                output = new TerminalOutput(fullPath);
-
-                break;
-            }
-
-            System.out.println(minDistNode.getPosition().getX() + " " + minDistNode.getPosition().getY());
         }
+        */
+
     }
 }
