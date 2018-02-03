91c3fa561584cce5af11a1d7d5b524ab3402f8c2
Shawn <sapols@yahoo.com>
2018-02-02 18:14:02 -0700

    Finish first implementation of a new Output that draws paths on maps.

--- src/main/java/mars/views/MapFrame.java
+++ src/main/java/mars/views/MapFrame.java
@@ -10,26 +10,62 @@ import java.io.File;
 import java.util.*;
 
 /**
- * A JFrame which holds and displays everything (the map with a path drawn on it, for now).
+ * A JFrame which holds and displays everything (just a JPanel with the map and a path drawn on it, for now).
  */
 public class MapFrame extends JFrame {
 
-    //TODO: map path for background needs to be passed in, not hard-coded
+    int frameWidth;
+    int frameHeight;
+    BufferedImage backgroundImage;
+    java.util.List<? extends Coordinate> path;
+
+    /**
+     * Constructor for this JFrame subclass.
+     * It sets the frame's title, the size of the frame as a specific fraction of the screen size,
+     * and declares that the program should end on hitting the close button.
+     * It then loads the map image used by our rover and gives it to the JPanel
+     * which manipulates and displays it.
+     *
+     * @param path The path which was output by our rover's algorithm
+     * @param mapImagePath The file path to the map used by our rover
+     */
     public MapFrame(java.util.List<? extends Coordinate> path, String mapImagePath) {
         try {
             setTitle("Algorithm Path Viewer");
+            this.path = path;
             Toolkit kit = Toolkit.getDefaultToolkit();
             Dimension screenSize = kit.getScreenSize();
-            setSize((int)(screenSize.width/2.2), (int)(screenSize.height/1.7)); //These dimensions could change
+            //TODO: maybe set size dimensions based on aspect ratio of map image?
+            frameWidth = (int)(screenSize.width/2.2);
+            frameHeight = (int)(screenSize.height/1.7);
+            setSize(frameWidth, frameHeight);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            //Add background image
-            /* BufferedImage backgroundImage = ImageIO.read(new File("./background.jpg")); */ //Works in .exe package if background.jpg is in the same folder as .exe
-            BufferedImage backgroundImage = ImageIO.read(new File(mapImagePath)); //Works in development
-            this.getContentPane().add(new MapPanel(this, path, backgroundImage));
+            //Add the (scrollable) map image to this frame
+            //TODO: use .jpg version instead of .tif to GREATLY shrink memory use
+            backgroundImage = ImageIO.read(new File(mapImagePath));
+            MapPanel imagePanel = new MapPanel(this, path, backgroundImage);
+            this.getContentPane().add(new ImageScroller(imagePanel));
             setVisible(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    /**
+     * A JScrollPane which allows us to scroll around large images.
+     * For convenience, it will load scrolled to the first (X,Y) coordinate in "path".
+     */
+    private class ImageScroller extends JScrollPane {
+        private ImageScroller(MapPanel imagePanel) {
+            super();
+
+            setPreferredSize(new Dimension(frameWidth, frameHeight));
+            getViewport().add(imagePanel); //Put the pane with the map image in this scroll pane
+
+            int firstX = path.get(0).getX() ;
+            int firstY = path.get(0).getY(); //Note that all the Y's in "path" have been altered by MapPanel.convertPathXYtoJavaXY()
+            getViewport().setViewPosition(new Point(firstX, firstY));
         }
     }
 
