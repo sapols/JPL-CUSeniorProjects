053ce02d65dc6c800f965b3a97b4fc54e477b914
Shawn <sapols@yahoo.com>
2018-02-03 17:21:46 -0700

    Manually resolve merge conflict with Dijkstra.java

--- src/main/java/mars/algorithm/Dijkstra.java
+++ src/main/java/mars/algorithm/Dijkstra.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Dijkstra extends Algorithm {
 
-    List<Coordinate> fullPath = new ArrayList<Coordinate>();
+    ArrayList<Coordinate> fullPath = new ArrayList<Coordinate>();
 
     /*
      * Default constructor for an Dijkstra.
@@ -19,6 +19,10 @@ public class Dijkstra extends Algorithm {
     public Dijkstra(MarsRover r) {
         rover = r;
         map = rover.getMap();
+    }
+
+    public ArrayList<Coordinate> getPath() {
+        return fullPath;
     }
 
     public void findPath(){
@@ -170,7 +174,7 @@ public class Dijkstra extends Algorithm {
                                 nodeVector.get(q).getPosition().getY() == neighborList.get(i).getPosition().getY()) {
                             // Alter node within vector.
                             List<Coordinate> tmpList = nodeVector.get(q).constructPath();
-                            fullPath = tmpList;
+                            fullPath = new ArrayList<Coordinate>(tmpList);
                             goalFound = true;
                         }
                     }
