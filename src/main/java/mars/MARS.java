1e601204cca1da75c15d5b627df8a1360c06d680
Robert Ballard <robert.ballard@colorado.edu>
2018-02-08 23:40:57 -0700

    remove testing code from main function

--- src/main/java/mars/MARS.java
+++ src/main/java/mars/MARS.java
@@ -15,24 +15,6 @@ public class MARS {
      * @param args unused
      */
     public static void main(String[] args) {
-        Coordinate startCoord = new Coordinate(7568,1507);
-        Coordinate endCoord = new Coordinate(7568,1727);
-        String mapPath = "src/main/resources/Phobos_Viking_Mosaic_40ppd_DLRcontrol.tif";
-        //String mapPath = "src/main/resources/Phobos_Viking_Mosaic_40ppd_DLRcontrol.tif";
-        MarsRover rover = new MarsRover(15 ,startCoord,endCoord,mapPath,8);
-        //Algorithm algorithm = new AlgorithmGreedy(rover,"limited");
-        //Algorithm algorithm = new AlgorithmUnlimitedScopeRecursive(rover);
-        //Algorithm algorithm = new AlgorithmUnlimitedScopeNonrecursive(rover);
-        //Algorithm algorithm = new AlgorithmLimitedScopeAStar(rover);
-        Algorithm algorithm = new Dijkstra(rover);
-        try {
-            algorithm.findPath();
-        } catch (Exception expectedException) {
-            //assertTrue("Failed to find a route it should have",false);
-        }
-        boolean check = true;
-        //assertTrue(check);
-
         TerminalInterface ti = new TerminalInterface();
         ti.promptUser();
     }
