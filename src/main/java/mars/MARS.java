258dc016738dfb3206956bcd3e4c0dc28b96b2cd
Robert Ballard <robert.ballard@colorado.edu>
2018-02-28 22:12:19 -0700

    remove testing code from main

--- src/main/java/mars/MARS.java
+++ src/main/java/mars/MARS.java
@@ -19,26 +19,6 @@ public class MARS {
      * @param args unused
      */
     public static void main(String[] args) throws Exception {
-        Coordinate startCoord = new Coordinate(7568,1507);
-        Coordinate endCoord = new Coordinate(7568,1727);
-        String mapPath = "src/main/resources/Phobos_Viking_Mosaic_40ppd_DLRcontrol.tif";
-        //String mapPath = "src/main/resources/Phobos_Viking_Mosaic_40ppd_DLRcontrol.tif";
-        MarsRover rover = new MarsRover(6 ,"L",startCoord,endCoord,mapPath,5);
-        //Algorithm algorithm = new AlgorithmGreedy(rover,"limited");
-        //Algorithm algorithm = new AlgorithmUnlimitedScopeRecursive(rover);
-        //Algorithm algorithm = new AlgorithmUnlimitedScopeNonrecursive(rover);
-        //Algorithm algorithm = new AlgorithmLimitedScopeAStar(rover);
-        //Algorithm algorithm = new AlgorithmLimitedDijkstra(rover,"MapImageOutput");
-        //Algorithm algorithm = new AlgorithmUnlimitedDijkstra(rover);
-        //Algorithm algorithm = new AlgorithmLimitedScopeAStar(rover,"MapImageOutput");
-        //Algorithm algorithm = new AlgorithmUnlimitedBestFirst(rover,"MapImageOutput");
-        Algorithm algorithm = new AlgorithmLimitedBreadthFirstSearch(rover,"MapImageOutput");
-        try {
-             algorithm.findPath();
-        } catch (Exception expectedException) {
-             //assertTrue("Failed to find a route it should have",false);
-        }
-        OutputFactory.getOutput(algorithm);
         TerminalInterface ti = new TerminalInterface();
         ti.promptUser();
     }
