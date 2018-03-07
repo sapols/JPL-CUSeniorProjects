c3a8919c9854d666b962f12536ff5a1783628906
joshuajenkins109 <joje3221@colorado.edu>
2018-03-07 16:22:05 -0700

    Start algorithm rover logic changed
    Startalgorithm no longer creates rover based on algtype but instead
    looks if fov is 0 or a usable number

--- src/main/java/mars/MARS.java
+++ src/main/java/mars/MARS.java
@@ -17,15 +17,26 @@ public class MARS {
 
 
         for( int i = 0; i < args.length; i++) {
+            if(args[i].compareTo("-s") == 0){
+                try{
+                    ti.slope = Double.parseDouble(args[i+1]);
+                }catch(Exception e){
+                    System.out.println("Warning: invalid input for slope");
+                }
+            }
             if(args[i].compareTo("-m") == 0){ //flag for map path
-                ti.mapPath = args[i+1];
+                try{
+                    ti.mapPath = args[i+1];
+                }catch(Exception e){
+                    System.out.println("Warning: Invalid input for mapPath");
+                }
             }
             if(args[i].compareTo("-sc") == 0){
                 try{
                     ti.startCoords = new Coordinate(Integer.parseInt(args[i+1]),Integer.parseInt(args[i+2]));
                     i = i+2;
                 }catch (Exception e) {
-                    System.out.println("Warning: Invalid Start Coordinates");
+                    System.out.println("Warning: Invalid input(s) for Start Coordinates");
                 }
             }
             if(args[i].compareTo("-ec") == 0){
@@ -33,11 +44,22 @@ public class MARS {
                     ti.endCoords = new Coordinate(Integer.parseInt(args[i+1]), Integer.parseInt(args[i+2]));
                     i = i + 2;
                 }catch(Exception e){
-                    System.out.println("Warning: Invalid End Coordinates");
+                    System.out.println("Warning: Invalid input(s) for End Coordinates");
+                }
+            }
+            if(args[i].compareTo("-fov") == 0){
+                try{
+                    ti.fieldOfView = Double.parseDouble(args[i+1]);
+                }catch(Exception e){
+                    System.out.println("Warning: Invalid Field of View input");
                 }
             }
             if(args[i].compareTo("-a") == 0){
-                ti.algorithmClass = args[i+1];
+                try {
+                    ti.algorithmClass = args[i + 1];
+                }catch(Exception e){
+                    System.out.println("Warning: Invalid input for algorithm class");
+                }
             }
             if(args[i].compareTo("-cop") == 0){
                     ti.coordType = "P";
@@ -46,8 +68,13 @@ public class MARS {
                 ti.coordType = "L";
             }
             if(args[i].compareTo("-o") == 0){
-                ti.outputClass = args[i+1];
+                try{
+                    ti.outputClass = args[i+1];
+                }catch(Exception e){
+                    System.out.println("Warning: Invalid input for output class");
+                }
             }
+
 
         }
         ti.promptUser();
