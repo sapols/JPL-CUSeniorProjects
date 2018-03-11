3734abdcd62735e41321b43426190799cf1654f2
robertBallard <robert.ballard@colorado.edu>
2018-03-11 16:22:53 -0600

    more work

--- src/main/java/mars/MARS.java
+++ src/main/java/mars/MARS.java
@@ -26,20 +26,25 @@ public class MARS {
     public static void main(String[] args) throws Exception{
         Coordinate startCoord = new Coordinate(7568,1507);
         Coordinate endCoord = new Coordinate(7568,1727);
-        String mapPath = "src/main/resources/Phobos_Viking_Mosaic_40ppd_DLRcontrol.tif";
+        String mapPath = "src/main/resources/Mars_Viking_MDIM21_Mosaic_global_232m.tif";
         ArrayList<String> algs = new ArrayList<String>();
 
+        MarsRover newRover = new MarsRover(90,"P",startCoord,endCoord,mapPath,1);
+        Algorithm algorithm = new LimitedGreedy(newRover, "MapImageOutput");
+        algorithm.getPath();
+        OutputFactory.getOutput(algorithm);
+
         algs.add("LimitedGreedy");
-        algs.add("LimitedAStar");
-        algs.add("LimitedBestFirst");
-        algs.add("LimitedBreadthFirstSearch");
-        algs.add("LimitedDijkstra");
-        algs.add("UnlimitedAStarNonRecursive");
-        algs.add("UnlimitedAStarRecursive");
-        algs.add("UnlimitedBestFirst");
-        algs.add("UnlimitedBreadthFirstSearch");
-        algs.add("UnlimitedDijkstra");
-        algs.add("UnlimitedGreedy");
+        //algs.add("LimitedAStar");
+        //algs.add("LimitedBestFirst");
+        //algs.add("LimitedBreadthFirstSearch");
+        //algs.add("LimitedDijkstra");
+        //algs.add("UnlimitedAStarNonRecursive");
+        //algs.add("UnlimitedAStarRecursive");
+        //algs.add("UnlimitedBestFirst");
+        //algs.add("UnlimitedBreadthFirstSearch");
+        //algs.add("UnlimitedDijkstra");
+        //algs.add("UnlimitedGreedy");
 
         doEval(algs, startCoord, endCoord, mapPath);
 
@@ -47,12 +52,11 @@ public class MARS {
         //ti.promptUser();
     }
 
-    public static ArrayList<Integer> eval(String algorithm, int slope, Coordinate startCoord, Coordinate endCoord, String mapPath, int fov) throws Exception{
+    public static ArrayList<Integer> eval(String algorithm, MarsRover newRover) throws Exception{
         long startTime, endTime, newTime;
         int newLength;
         ArrayList<Long> times = new ArrayList<Long>();
         ArrayList<Integer> lengths = new ArrayList<Integer>();
-        MarsRover newRover = new MarsRover(slope,"P", startCoord, endCoord, mapPath, fov);
         for(int i = 1; i < 11; i++){
             Algorithm newAlgorithm = AlgorithmFactory.getAlgorithm(algorithm,newRover,"TerminalOutput");
             startTime = System.nanoTime();
@@ -93,13 +97,17 @@ public class MARS {
             }
         }
         System.out.println("Header written");
+        MarsRover rover = new MarsRover(0,"P",startCoord,endCoord,mapPath,0);
+        System.out.println("Rover instantiated");
         for(String algorithmName : algorithms){
             outFile += "\n" + algorithmName + ",";
             for(int i = 0; i < 13; i++) {
                 for (int j = 0; j < 4; j++) {
                     System.out.println("Testing alg " + algorithmName + " with slope " + (7 + i*2) + " fov " + (10 + j*10) + " (Test " + testsDone + "/" + testsTotal + ")");
                     testsDone += 1;
-                    outResults = eval(algorithmName, (7 + i*2), startCoord, endCoord, mapPath, (10 + j*10));
+                    rover.setMaxSlope(7+i*2);
+                    rover.setFieldOfView(10+j*10);
+                    outResults = eval(algorithmName, rover);
                     outFile += outResults.get(0) + "," + outResults.get(1) + ",";
                 }
             }
