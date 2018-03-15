9b749cc93edec3a0c6bbfbcbce8eb21ea7174ce9
Robert Ballard <robert.ballard@colorado.edu>
2018-03-15 13:20:25 -0600

    best config ever

--- src/main/java/mars/MARS.java
+++ src/main/java/mars/MARS.java
@@ -24,30 +24,34 @@ public class MARS {
      * @param args unused
      */
     public static void main(String[] args) throws Exception{
-        Coordinate startCoord = new Coordinate(670,6565);
-        Coordinate endCoord = new Coordinate(425,6280);
+        Coordinate startCoord = new Coordinate(955,6350);
+        Coordinate endCoord = new Coordinate(915,5950);
         String mapPath = "src/main/resources/mi15S158E.tif";
         ArrayList<String> algs = new ArrayList<String>();
 
-        //MarsRover rover = new MarsRover(8,"P",startCoord,endCoord,mapPath,31);
-        //Algorithm alg = new LimitedAStar(rover,"TerminalOutput");
+        ///*
+        MarsRover rover = new MarsRover(6,"P",startCoord,endCoord,mapPath,10);
+        Algorithm alg = new LimitedAStar(rover,"MapImageOutput");
 
-        //alg.findPath();
-        //OutputFactory.getOutput(alg);
+        alg.findPath();
+        OutputFactory.getOutput(alg);
+        //*/
 
-        algs.add("LimitedGreedy");
+        /*
+        //algs.add("LimitedGreedy");
         algs.add("LimitedAStar");
-        algs.add("LimitedBestFirst");
-        algs.add("LimitedBreadthFirstSearch");
-        algs.add("LimitedDijkstra");
+        //algs.add("LimitedBestFirst");
+        //algs.add("LimitedBreadthFirstSearch");
+        //algs.add("LimitedDijkstra");
         //algs.add("UnlimitedAStarNonRecursive");
-        algs.add("UnlimitedAStarRecursive");
-        algs.add("UnlimitedBestFirst");
+        //algs.add("UnlimitedAStarRecursive");
+        //algs.add("UnlimitedBestFirst");
         //algs.add("UnlimitedBreadthFirstSearch");
         //algs.add("UnlimitedDijkstra");
-        algs.add("UnlimitedGreedy");
+        //algs.add("UnlimitedGreedy");
 
         doEval(algs, startCoord, endCoord, mapPath);
+        */
 
         //TerminalInterface ti = new TerminalInterface();
         //ti.promptUser();
@@ -58,7 +62,7 @@ public class MARS {
         int newLength;
         ArrayList<Long> times = new ArrayList<Long>();
         ArrayList<Integer> lengths = new ArrayList<Integer>();
-        for(int i = 1; i < 11; i++){
+        for(int i = 1; i < 2; i++){
             Algorithm newAlgorithm = AlgorithmFactory.getAlgorithm(algorithm,newRover,"TerminalOutput");
             startTime = System.nanoTime();
             newAlgorithm.findPath();
